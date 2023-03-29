143a6cd12cb6c470f2be00ebefc198cf1035116f
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-03-28 20:36:28 -0400

    commit tree 7b6ee2e9cf0160153d205d28187494f1ab8c2bee

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,14 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+  only_food = spicy_foods.map{|food| food[:name]}
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+  very_spicy = spicy_foods.select{|food| food[:heat_level] > 5}
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -28,18 +30,26 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+  spicy_foods.each do |food|
+    puts "#{food[:name]}" " (#{food[:cuisine]}) " '| Heat Level: ' "#{"🌶" * food[:heat_level]}"
+  end
 end
+
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
+  selected_cuisine = spicy_foods.find {|food| food[:cuisine] == cuisine}
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
   # your code here
+  spiciest = spicy_foods.sort do |food1, food2|
+    food1[:heat_level] <=> food2[:heat_level]
+  end
 end
 
 # given an array of spicy foods, output to the terminal ONLY 
@@ -48,10 +58,14 @@ end
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
   # your code here
+  last_spice = spiciest_foods(spicy_foods)
+  print_spicy_foods(last_spice)
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
   # your code here
+  food_sum = spicy_foods.sum {|food| food[:heat_level]} / spicy_foods.length
+
 end
